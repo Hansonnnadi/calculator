55fa1aca23276a1f06c16681046876dee5aec198
Hanson Nnadi <hanson2510@gmail.com>
2022-06-25 13:29:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const num1 = parseFloat(prompt("Entre first number"));
+const num2 = parseFloat(prompt("Entre second number"));
+const operator = prompt("Enter operator (+,-,/,*)");
+//if(isNaN(num1)|| isnan(num2)){
+//alert('Wrong input! please Refresh the page again');    
+//} else{
+    let result = num1 = num2;
+    if(operator == "+") {
+        result = num1 = num2;
+    } else if (operator =="-") {
+        result = num1 = num2;
+    } else if (operator =="-") {
+        result = num1 = num2;
+    } else if (operator =="-") {
+        result = num1 = num2;
+    
+    }
+    alert(num1 + operator + num2 +'='+ result);
+    //}
